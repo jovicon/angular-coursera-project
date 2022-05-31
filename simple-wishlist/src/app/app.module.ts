3c69587e9c6cfdf5bf8101e5ba68149b80a57efd
Jose Contreras <jocontrera16@gmail.com>
2022-05-31 09:15:22 -0400

    added: routing ready

--- simple-wishlist/src/app/app.module.ts
+++ simple-wishlist/src/app/app.module.ts
@@ -1,22 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TripDestinationComponent } from './trip-destination/trip-destination.component';
 import { ListDestinationComponent } from './list-destination/list-destination.component';
+import { DestinationDetailComponent } from './destination-detail/destination-detail.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: ListDestinationComponent,
+  },
+  {
+    path: 'destiny',
+    component: DestinationDetailComponent,
+  },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TripDestinationComponent,
-    ListDestinationComponent
+    ListDestinationComponent,
+    DestinationDetailComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ simple-wishlist/src/app/destination-detail/destination-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destination-detail',
+  templateUrl: './destination-detail.component.html',
+  styleUrls: ['./destination-detail.component.scss']
+})
+export class DestinationDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

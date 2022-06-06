9ab4a8f42d6335515024a686d3ea04c1b698744c
Jose Contreras <jocontrera16@gmail.com>
2022-06-06 19:34:15 -0400

    added: using validations

--- simple-wishlist/src/app/form-trip-destination/form-trip-destination.component.ts
+++ simple-wishlist/src/app/form-trip-destination/form-trip-destination.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 import { TripDestination } from '../models/trip-destination.models';
 
@@ -11,11 +16,12 @@ import { TripDestination } from '../models/trip-destination.models';
 export class FormTripDestinationComponent implements OnInit {
   @Output() onItemAdded: EventEmitter<TripDestination>;
   fg: FormGroup;
+  minLong: number = 5;
 
   constructor(fb: FormBuilder) {
     this.onItemAdded = new EventEmitter();
     this.fg = fb.group({
-      name: ['', Validators.required],
+      name: ['', Validators.compose([Validators.required, this.nameValidator])],
       url: ['', Validators.required],
     });
   }
@@ -26,5 +32,13 @@ export class FormTripDestinationComponent implements OnInit {
     const destination = new TripDestination(name, url);
     this.onItemAdded.emit(destination);
     return false;
+  };
+
+  nameValidator = (control: FormControl) => {
+    const long = control.value.toString().trim().length;
+    const isValid = !(long > 0 && long < this.minLong);
+
+    console.log(`nameValidator is valid: ${isValid}`);
+    return isValid ? null : { invalidName: !isValid };
   };
 }

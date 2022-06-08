49d4e7a56d452c8402d3d76d72e45e1df433e8ce
Jose Contreras <jocontrera16@gmail.com>
2022-06-07 21:53:32 -0400

    added: redux part 1

--- simple-wishlist/src/app/models/trip-destination.models.ts
+++ simple-wishlist/src/app/models/trip-destination.models.ts
@@ -1,9 +1,11 @@
 export class TripDestination {
   private selectec: boolean = false;
   public services: string[];
+  public id: string;
 
   constructor(public name: string, public imageUrl: string) {
     this.services = ['pool', 'restaurant', 'bar'];
+    this.id = name.toLowerCase().replace(/ /g, '-') + '-' + Math.random();
   }
 
   isSelected(): boolean {
